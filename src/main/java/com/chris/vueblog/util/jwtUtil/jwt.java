6adfba611598c351f34cdfe40738a9ec2f048903
chenlinhe <clhhhhhh@16.com>
2021-01-27 17:30:05 +0800

    新增jwt
    Signed-off-by: chenlinhe <clhhhhhh@16.com>

---
+++ src/main/java/com/chris/vueblog/util/jwtUtil/jwt.java
@@ -0,0 +1,13 @@
+package com.chris.vueblog.util.jwtUtil;
+
+public class jwt {
+    public static void main(String[] args) {
+
+    }
+
+    // token由三部分组成，
+    // 1. header
+    // 2. payload
+    // 3. sign
+
+}
